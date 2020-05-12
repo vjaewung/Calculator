8d91864c71300df6de34dc3e631ddcdc08c7863c
vjaewung <vjaewung@gmail.com>
2020-05-13 02:08:11 +0900

    1st Commit

--- src/main/java/com/example/calculator/MainActivity.java
+++ src/main/java/com/example/calculator/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isFirstInput = true;
+    int resultNumber = 0;
+    char operator = '+';
+
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resultText = findViewById(R.id.result_text);
     }
+
+
 }
